34b514e9803cf36969ade96939887311dd814391
Ahmad Yasser <ahmad.yasser@yassir.com>
2024-01-20 00:50:20 +0200

    Initial Commit

---
+++ codecov-demo/codecov_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  codecov_demoApp.swift
+//  codecov-demo
+//
+//  Created by Ahmad Yasser on 20/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct codecov_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ codecov-demoUITests/codecov_demoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  codecov_demoUITestsLaunchTests.swift
+//  codecov-demoUITests
+//
+//  Created by Ahmad Yasser on 20/01/2024.
+//
+
+import XCTest
+
+final class codecov_demoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
